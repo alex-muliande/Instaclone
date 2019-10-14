4885a9bf96c5d94ff057875d441c17231a6a6384
alex-muliande <amuliande@gmail.com>
2019-10-14 20:53:11 +0300

    Add crispy_forms to the apps,add redirect log in it the settings

--- Instaclone/settings.py
+++ Instaclone/settings.py
@@ -33,6 +33,7 @@ ALLOWED_HOSTS = []
 INSTALLED_APPS = [
     'insta.apps.InstaConfig',
     'users.apps.UsersConfig',
+    'crispy_forms',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
@@ -124,3 +125,8 @@ USE_TZ = True
 STATIC_URL = '/static/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 MEDIA_URL = '/media/'
+
+CRISPY_TEMPLATE_PACK = 'bootstrap4'
+
+LOGIN_REDIRECT_URL = 'insta-home'
+LOGIN_URL = 'login'
