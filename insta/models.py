e67b451d147c0dc93b87e1aac6a075b0c43ed822
alex-muliande <amuliande@gmail.com>
2019-10-14 12:38:48 +0300

    Add  post model

--- insta/models.py
+++ insta/models.py
@@ -3,17 +3,21 @@ from django.contrib.auth.models import User
 
 #   your models here.
 class Profile(models.Model):
-    user = models.OneToOneField(User, on_delete=models.CASCADE, related_name='profile')
-    Name = models.TextField(default="Anonymous")
+    user = models.OneToOneField(User, on_delete=models.CASCADE)
+  
     profile_picture = models.ImageField(upload_to='users/', default='users/user.png')
     bio = models.TextField(default="Welcome Me!")
+
+    def __str__(self):
+        return f'{self.user.username} Profile' 
 
     
 
 
 class Post(models.Model):
+    image_name = models.CharField(max_length = 30)
     image = models.ImageField(upload_to='posts/')
-    user = models.ForeignKey(Profile, on_delete=models.CASCADE)
+    profile = models.OneToOneField(Profile, on_delete=models.CASCADE)
 
     def __str__(self):
-        return self.user
+        return self.image_name
