3bf391d0b8c0efe8135c86ea81c8861ef3aeef23
alex-muliande <amuliande@gmail.com>
2019-10-14 12:37:50 +0300

    Add imports and settings for url patterns

--- Instaclone/urls.py
+++ Instaclone/urls.py
@@ -15,9 +15,18 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path,include
+from django.conf import settings
+from django.conf.urls.static import static
+from users import views as user_views
 
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('register/', user_views.register, name='register'),
     path('', include ('insta.urls')),
 ]
+
+if settings.DEBUG:
+    urlpatterns+=static(settings.MEDIA_URL,document_root=settings.MEDIA_ROOT)
+
+
