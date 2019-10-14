4f673952143e55298f36a8fdcb75fd864c98ad6b
alex-muliande <amuliande@gmail.com>
2019-10-14 20:47:52 +0300

    Add login required decorator to the log in profile

--- users/views.py
+++ users/views.py
@@ -1,14 +1,21 @@
 from django.shortcuts import render,redirect
-from django.contrib.auth.forms import UserCreationForm
 from django.contrib import messages
+from django.contrib.auth.decorators import login_required
+from .forms import UserRegisterForm
 # our views here.
 def register (request):
     if request.method == 'POST':
-        form = UserCreationForm(request.POST)
+        form = UserRegisterForm(request.POST)
         if form.is_valid():
+            form.save()
             username= form.cleaned_data.get('username')
             messages.success(request, f'Account created for {username}!')
-            return redirect('insta-home')
+            return redirect('login')
     else:
-         form = UserCreationForm()
+         form = UserRegisterForm()
     return render(request,'registration_form.html', {'form':form})
+
+@login_required
+def profile(request):
+    return render(request, 'profile.html')
+
