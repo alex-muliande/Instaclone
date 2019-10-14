689f35c42fb99b613635d1956fcb2baef2f60e03
alex-muliande <amuliande@gmail.com>
2019-10-14 12:39:13 +0300

    Remove dammy data of post

--- insta/views.py
+++ insta/views.py
@@ -2,21 +2,10 @@ from django.shortcuts import render
 from django.http import HttpResponse
 from .models import Post
 
-posts=[
-    {
-        'user': 'Alex',
-        'image': 'imagehttps://cdn.pixabay.com/photo/2019/09/04/02/52/road-4450611__340.jpg',
-        'caption': 'things by me',
-    },
-    {
-        'user': 'Alphy',
-        'image': '........',
-        'caption': 'things by us',
-    }
-]
 
 # Create your views here.
 def home(request):
+    posts = Post.objects.all()
     context ={
         'posts': posts
     }
