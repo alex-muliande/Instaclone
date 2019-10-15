4fde5d0ff4a9239ad5a6abd99bb6c84ed9a8ff90
alex-muliande <amuliande@gmail.com>
2019-10-15 08:01:21 +0300

    Add  paths for the postview

--- insta/urls.py
+++ insta/urls.py
@@ -1,8 +1,10 @@
 from django.conf import settings
 from django.conf.urls.static import static
 from django.urls import path
+from .views import PostListView
 from . import views
 
 urlpatterns = [
-    path('', views.home, name='insta-home'),
+    path('', PostListView.as_view(), name='insta-home '),
+    path('post/<int:pk>/', PostListView.as_view(), name='post-detail'),
 ]
