7b950c318b5d02bc8ac7ba1f662de1472908f0aa
alex-muliande <amuliande@gmail.com>
2019-10-15 08:00:40 +0300

    Add a PostListView  class

--- insta/views.py
+++ insta/views.py
@@ -1,4 +1,5 @@
 from django.shortcuts import render
+from django.views.generic import ListView
 from django.http import HttpResponse
 from .models import Post
 
@@ -10,3 +11,9 @@ def home(request):
         'posts': posts
     }
     return render(request, 'index.html',context)  
+
+
+class PostListView(ListView):
+    model = Post
+    template_name = 'index.html'
+    context_object_name = 'posts'
