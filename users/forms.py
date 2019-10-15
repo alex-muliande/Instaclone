f11bd370f4ca02c480ed1a4682c36a2a19c56c5e
alex-muliande <amuliande@gmail.com>
2019-10-15 07:59:42 +0300

    Add class meta for the profile update form

--- users/forms.py
+++ users/forms.py
@@ -1,6 +1,7 @@
 from django import forms
 from django.contrib.auth.models import User
 from django.contrib.auth.forms import UserCreationForm
+from insta.models import Profile
 
 
 class UserRegisterForm(UserCreationForm):
@@ -9,3 +10,15 @@ class UserRegisterForm(UserCreationForm):
     class Meta:
         model = User
         fields = ['username', 'email']
+
+class UserUpdateForm(forms.ModelForm):
+    email = forms.EmailField()
+
+    class Meta:
+        model = User
+        fields = ['username', 'email']
+
+class ProfileUpdateForm(forms.ModelForm):
+    class Meta:
+        model = Profile
+        fields = ['profile_picture']
