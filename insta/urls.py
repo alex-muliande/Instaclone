40761de525eb9750f086c4914318fc481e8fe42b
alex-muliande <amuliande@gmail.com>
2019-10-16 10:54:06 +0300

    Add url paths post for update ans creating new apps

--- insta/urls.py
+++ insta/urls.py
@@ -1,10 +1,14 @@
 from django.conf import settings
 from django.conf.urls.static import static
 from django.urls import path
-from .views import PostListView
+from django.conf.urls import url
+from .views import PostListView,PostDetailView,PostCreateView,PostUpdateView
 from . import views
 
 urlpatterns = [
-    path('', PostListView.as_view(), name='insta-home '),
-    path('post/<int:pk>/', PostListView.as_view(), name='post-detail'),
+    path('', PostListView.as_view(), name='insta-home'),
+    path('post/<int:pk>/', PostDetailView.as_view(), name='post-detail'),
+    path('post/<int:pk>/update/', PostUpdateView.as_view(), name='post-update'),
+    path('post/new/', PostCreateView.as_view(), name='post-create'),
+    url(r'^like/(\d+)',views.likePost, name="likePost"),
 ]
